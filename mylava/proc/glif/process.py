bd16502b173b8aa5a238e264f4a64c4984b875e9
jacobpennington <jacob.pennington@wsu.edu>
2022-09-21 10:18:36 -0700

    Added sketch of initial test routine.

--- mylava/proc/glif/process.py
+++ mylava/proc/glif/process.py
@@ -4,4 +4,17 @@ from lava.magma.core.process.ports.ports import InPort, OutPort
 
 
 class GLIF(AbstractProcess):
-    pass
+
+    def print_variables(self):
+    """Prints all variables of a GLIF process and their values."""
+    # TODO: Currently copied from the tutorial LIF, will need to be updated
+    #       to match the new variable names. Just putting this here to remind
+    #       myself to include a similar method for debugging/testing.
+    sp = 3 * "  "
+    print("Variables of the LIF:")
+    print(sp + "u:    {}".format(str(self.u.get())))
+    print(sp + "v:    {}".format(str(self.v.get())))
+    print(sp + "du:   {}".format(str(self.du.get())))
+    print(sp + "dv:   {}".format(str(self.dv.get())))
+    print(sp + "bias: {}".format(str(self.bias.get())))
+    print(sp + "vth:  {}".format(str(self.vth.get())))
