7dd681c2df425f0fceb3e85ee2deb129c5bde94f
jacobpennington <jacob.pennington@wsu.edu>
2022-09-21 16:24:16 -0700

    Fleshed out GLIF implementation, more notes

--- mylava/proc/glif/models.py
+++ mylava/proc/glif/models.py
@@ -20,39 +20,70 @@ class PyGlifModelCPU(PyLoihiProcessModel):
     spikes_out: PyOutPort = LavaPyType(PyOutPort.VEC_DENSE, bool, precision=1)
     current: np.ndarray = LavaPyType(np.ndarray, float)
     voltage: np.ndarray = LavaPyType(np.ndarray, float)
-    # v_time_constant: np.ndarray = LavaPyType(float, float)
-    u_time_constant: np.ndarray = LavaPyType(float, float)
+    current_time_constant: np.ndarray = LavaPyType(float, float)
 
     # can be float if same for all neurons
+    # alternatively, will broadcasting work? i.e. will array with shape (1,)
+    # be interpreted as "apply same to all neurons"?
     capacitance: np.ndarray = LavaPyType(np.ndarray, float)
     resistance: np.ndarray = LavaPyType(np.ndarray, float)
     # TODO: multiple thresholds
-    threshold: np.ndarray = LavaPyType(float, float)
-    reset: np.ndarray = LavaPyType(float, float)
-    resting: np.ndarray = LavaPyType(float, float)
-    after_spike_currents: np.ndarray = LavaPyType(float, float)
-
+    threshold: np.ndarray = LavaPyType(np.ndarray, float)
+    reset: np.ndarray = LavaPyType(np.ndarray, float)
+    resting: np.ndarray = LavaPyType(np.ndarray, float)
+    # TODO: how to implement refractory period?
+    #       "After a refractory period, δt, the state variables are updated
+    #        with a linear dependence on the state before the spike"
+    refractory_period: np.ndarray = LavaPyType(np.ndarray, float)
+    # TODO: make these shape (n_1, ..., n_m, J) where n_i are the neuron shape
+    #       (usually just n_1) and J is the number of post-spike currents
+    #       (2 for now to match teeter et al)
+    after_spike_currents: np.ndarray = LavaPyType(np.ndarray, float)
+    last_AS_currents: np.ndarray = LavaPyType(np.ndarray, float)
+    #       Paper actually uses 1/time_constant, so either need to specify
+    #       inverse in argument to model or add 1/ in code. I'm leaning toward
+    #       the former to avoid extra computations, but that might be unintuitive
+    #       in terms of parameter specification.
+    after_spike_time_constants: np.ndarray = LavaPyType(np.ndarray, float)
+    after_spike_amplitude: np.ndarray = LavaPyType(np.ndarray, float)
 
     def run_spk(self):
+        # This gets run during every spike phase of the simulation
         data_in = self.amplitude_in.recv()
-
-        # TODO: this is the Loihi 1 decay rule, what should glif be?
-        self.current[:] = self.current * (1 - self.u_time_constant)
-        self.current[:] += data_in  # TODO: how is this weighted?
-        self.voltage[:] = self._new_voltage()
-
+        self._update_currents(data_in)
+        self._update_voltage()
         # TODO: add other thresholds, they get updated too
         spikes_out = self.voltage >= self.threshold
-        self.voltage[spikes_out] = 0  # Reset voltage to 0
+        self._process_spikes(spikes_out)
         self.spikes_out.send(spikes_out)
 
-    def _new_voltage(self):
-        dv = (self.current + self._after_spike() - self._leak())
-        return (1/self.capacitance)*dv
+    def _update_currents(self, data_in):
+        # TODO: this is the Loihi 1 decay rule, what should glif be?
+        self.current[...] *= (1 - self.current_time_constant)
+        self.current[...] += data_in  # TODO: how is this weighted?
+        self.last_AS_currents[...] = self.after_spike_currents
+        self.after_spike_currents[...] *= (1 - self.after_spike_time_constants)
+
+    def _update_voltage(self):
+        dv = (self.current + self._after_spike_currents() - self._leak())
+        self.voltage[...] = (1/self.capacitance)*dv
 
     def _leak(self):
         return (1/self.resistance)*(self.voltage - self.resting)
 
-    def _after_spike(self):
-        # TODO: need to update them first
-        return sum(self.after_spike_currents)
+    def _after_spike_currents(self):
+        # TODO: I'm assuming numpy operations aren't allowed on the Loihi 2
+        #       version, so would have to do this with for loop(s) I guess?
+        return np.sum(self.after_spike_currents, axis=1)
+
+    def _process_spikes(self, spikes_out):
+        self.voltage[spikes_out] = 0  # Reset voltage to 0
+        # TODO:
+        # f_j X I_j(t_-) + delta I_j
+        # "f is a fraction of the current implemented after a spike; here it is
+        # always set to 1. delta I_j is the amplitude of the spike-induced
+        # currents"
+        # but from supplementary:
+        # "fj = exp (−k_j δt)", where delta_t is "spike cut length"
+        # Setting to 1 for now
+        self.after_spike_currents += (self.last_AS_current + self.after_spike_amplitude)
--- mylava/proc/glif/process.py
+++ mylava/proc/glif/process.py
@@ -4,17 +4,59 @@ from lava.magma.core.process.ports.ports import InPort, OutPort
 
 
 class GLIF(AbstractProcess):
+    
+    def __init__(self, shape=(1,), one_time_constant=True, initial_current=0.0,
+                 initial_voltage=0.0, current_time_constant=0.0, capacitance=0.0,
+                 resistance=0.0, threshold=0.0, reset=0.0, resting=0.0,
+                 refractory_period=0.0, n_AS_currents=2, initial_AS_currents=0.0,
+                 after_spike_time_constants=0.0, after_spike_amplitude=0.0,
+                 **kwargs):
+        super().__init__()
+        # TODO: Lava tutorials use a "blah = kwargs.pop('blah', default)"
+        #       scheme. Is this actually important, or is using standard
+        #       keyword arguments like this acceptable?
+        # TODO: reasonable default values
+        as_shape = shape + (n_AS_currents,)
+        if one_time_constant:
+            tc_shape = (1,)
+            as_tc_shape = (1, n_AS_currents)
+        else:
+            tc_shape = shape
+            as_tc_shape = as_shape
+
+        self.amplitude_in = InPort(shape=shape)
+        self.spikes_out = OutPort(shape=shape)
+        self.current = Var(shape=shape, init=initial_current)  
+        self.voltage = Var(shape=shape, init=initial_voltage)
+        self.current_time_constant = Var(
+            shape=tc_shape, init=current_time_constant
+            )
+        # TODO: option to have these be shape (1,) similar to time constants?
+        self.capacitance = Var(shape=shape, init=capacitance)
+        self.resistance = Var(shape=shape, init=resistance)
+        self.threshold = Var(shape=shape, init=threshold)
+        self.reset = Var(shape=shape, init=reset)
+        self.resting = Var(shape=shape, init=resting)
+        self.refractory_period = Var(shape=shape, init=refractory_period)
+
+        self.after_spike_currents = Var(shape=as_shape, init=initial_AS_currents)
+        self.last_AS_currents = Var(shape=as_shape, init=0.0)
+        self.after_spike_time_constants = Var(
+            shape=as_tc_shape, init=after_spike_time_constants
+            )
+        self.after_spike_amplitude = Var(shape=as_shape, init=after_spike_amplitude)
+
 
     def print_variables(self):
-    """Prints all variables of a GLIF process and their values."""
-    # TODO: Currently copied from the tutorial LIF, will need to be updated
-    #       to match the new variable names. Just putting this here to remind
-    #       myself to include a similar method for debugging/testing.
-    sp = 3 * "  "
-    print("Variables of the LIF:")
-    print(sp + "u:    {}".format(str(self.u.get())))
-    print(sp + "v:    {}".format(str(self.v.get())))
-    print(sp + "du:   {}".format(str(self.du.get())))
-    print(sp + "dv:   {}".format(str(self.dv.get())))
-    print(sp + "bias: {}".format(str(self.bias.get())))
-    print(sp + "vth:  {}".format(str(self.vth.get())))
+        """Prints all variables of a GLIF process and their values."""
+        # TODO: Currently copied from the tutorial LIF, will need to be updated
+        #       to match the new variable names. Just putting this here to remind
+        #       myself to include a similar method for debugging/testing.
+        sp = 3 * "  "
+        print("Variables of the LIF:")
+        print(sp + "u:    {}".format(str(self.u.get())))
+        print(sp + "v:    {}".format(str(self.v.get())))
+        print(sp + "du:   {}".format(str(self.du.get())))
+        print(sp + "dv:   {}".format(str(self.dv.get())))
+        print(sp + "bias: {}".format(str(self.bias.get())))
+        print(sp + "vth:  {}".format(str(self.vth.get())))
